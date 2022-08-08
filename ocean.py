c378a5653aff62043e8288bdd68c1549788ef4ee
DataAnalysisSOH <105328921+DataAnalysisSOH@users.noreply.github.com>
2022-08-08 17:23:42 -0400

    praticing

--- ocean.py
+++ ocean.py
@@ -1,2 +1,2 @@
 print("The Ocean has its silent caves,Deep, quiet, and alone;Though there be fury on the waves,Beneath them there is none.")
-print("")
+print("Calmly the wearied seamen rest Beneath their own blue sea. The ocean solitudes are blest, For there is purity.")
